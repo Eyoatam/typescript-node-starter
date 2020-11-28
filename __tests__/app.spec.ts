39ca377eecf6c2c014836d3c944bb511304073ab
Eyoatam <eyoatamtamirat7@gmail.com>
2020-11-28 13:56:40 +0300

    fix tests

--- __tests__/app.spec.ts
+++ __tests__/app.spec.ts
@@ -1,14 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { jest } from "@jest/globals";
+
+jest.useFakeTimers();
+
+// etc.
+
 test("starts the app", () => {
-	const express = require("express");
-	const cors = require("cors");
-	const bodyParser = require("body-parser");
 	const app = express();
 
-	app.use(cors());
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(bodyParser.json());
 	app.use((req, res, next) => {
 		console.log("request acepted");
 		res.status(200).json({ messgae: "request accepted" });
+		next();
 	});
 });
