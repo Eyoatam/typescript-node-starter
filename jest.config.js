cec15c699e2195609e279334bad37939fae4df71
Eyoatam <eyoatamtamirat7@gmail.com>
2021-01-08 14:52:38 +0300

    update jestconfig

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,6 @@ module.exports = {
 	transform: {
 		"^.+\\.(ts|tsx)$": "ts-jest",
 	},
-	testMatch: ["**/test/**/*.test.(ts|js)"],
+	testMatch: ["**/__tests__/**/*.spec.(ts|js)"],
 	testEnvironment: "node",
 };
